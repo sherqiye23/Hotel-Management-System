5efde7f07757270747ff2b86edf9089f39cf35de
Şərqiyə <145263854+sherqiye23@users.noreply.github.com>
2025-11-29 22:33:35 +0400

    created user profile page and components

---
+++ src/app/(main)/profile/page.tsx
@@ -0,0 +1,11 @@
+import UserProfile from "@/src/components/Profile components/UserProfile";
+
+export const metadata = {
+    title: 'Profile - Hotel Management System',
+};
+
+export default function ProfilePage() {
+  return (
+    <UserProfile />
+  )
+}
